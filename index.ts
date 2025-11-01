3807df3f5f598b1fab10b6dcf4d7a7a0510f2874
DrMint <29893320+DrMint@users.noreply.github.com>
2025-11-01 14:08:09 +0100

    Added health check handler

--- index.ts
+++ index.ts
@@ -1,12 +1,14 @@
 import { Endpoint } from "@/handlers/handler";
 import { gitHubPushHandler } from "@/handlers/github-push-handler";
 import { fallbackHandler } from "@/handlers/fallback-handler";
+import { healthCheckHandler } from "@/handlers/health-check-handler";
 import { getArgs } from "@/parse-args";
 
 const args = getArgs();
 
 const endpoint = new Endpoint(fallbackHandler);
 endpoint.addHandler(gitHubPushHandler);
+endpoint.addHandler(healthCheckHandler);
 
 const server = Bun.serve({
   port: args.port,
